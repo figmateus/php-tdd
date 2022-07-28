868a53182b83c83ba4c2e080b5bd42326caf786e
Mateus Figueredo <mateusfigueredo037@gmail.com>
2022-07-28 14:16:04 -0300

    add src folder

---
+++ src/Produto.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Code;
+
+class Produto {
+
+    private $name;
+    private $price;
+    private $slug;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    public function setSlug($slug): void
+    {
+        $this->slug = $slug;
+    }
+}
